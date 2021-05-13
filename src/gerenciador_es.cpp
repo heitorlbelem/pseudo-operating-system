40577515222f4f8215ef07a96afc71eb01696e8c
thiagolrpinho <thiagoluisrpinho@gmail.com>
2021-05-13 18:24:57 -0300

    :sparkles: add: measuring fcfs distance

--- src/gerenciador_es.cpp
+++ src/gerenciador_es.cpp
@@ -4,6 +4,7 @@
 #include <streambuf>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 std::ifstream t("file.txt");
 
@@ -31,15 +32,36 @@ void GerenciadorES::imprimeDistanciaPercorrida(std::string nomeArquivo )
     size_t pos = 0;
     std::vector<int> enderecosCilindrosListados;
     std::string token;
-    int numeroToken;
+    int numero_token, ultimo_endereco_visitado;
+    int distancia_fcfs, distancia_sstf, distancia_scan;
+    int tamanho_disco;
+
     while ((pos = s.find(delimitador)) != std::string::npos) {
         token = s.substr(0, pos);
-        numeroToken = atoi(token.c_str());
-        enderecosCilindrosListados.push_back(numeroToken);
+        numero_token = atoi(token.c_str());
+        enderecosCilindrosListados.push_back(numero_token);
         s.erase(0, pos + delimitador.length());
     }
-    numeroToken = atoi(s.c_str());
-    enderecosCilindrosListados.push_back(numeroToken);
-    std::cout << enderecosCilindrosListados.size() << std::endl;
+    numero_token = atoi(s.c_str());
+    enderecosCilindrosListados.push_back(numero_token);
+    tamanho_disco = enderecosCilindrosListados.at(0);
+    enderecosCilindrosListados.erase(enderecosCilindrosListados.begin());
+    // Calculando distancia FCFS
+    distancia_fcfs = 0;
+    ultimo_endereco_visitado = -1;
+    for(std::vector<int>::iterator it = enderecosCilindrosListados.begin();
+    it != enderecosCilindrosListados.end(); 
+     ++it) {
+        numero_token = *it;
+        if( ultimo_endereco_visitado == -1)
+        {
+            ultimo_endereco_visitado = numero_token;
+        } else {
+            distancia_fcfs += abs(numero_token - ultimo_endereco_visitado);
+            ultimo_endereco_visitado = numero_token;
+        }
+    }
 
+  
+    std::cout << "FCFS\t" << distancia_fcfs << std::endl;
 }
