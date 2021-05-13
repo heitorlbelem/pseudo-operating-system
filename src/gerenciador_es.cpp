199361001cd4f380410aafb2c0fea831f66b2845
thiagolrpinho <thiagoluisrpinho@gmail.com>
2021-05-13 17:40:30 -0300

    :sparkles: add: storing cylinder adresses as vector
    Signed-off-by: thiagolrpinho <thiagoluisrpinho@gmail.com>

--- src/gerenciador_es.cpp
+++ src/gerenciador_es.cpp
@@ -3,12 +3,13 @@
 #include <fstream>
 #include <streambuf>
 #include <iostream>
+#include <vector>
 
 std::ifstream t("file.txt");
 
 // construtor
 GerenciadorES::GerenciadorES() {
-    
+
 }
 // destrutor
 GerenciadorES::~GerenciadorES() {
@@ -16,9 +17,29 @@ GerenciadorES::~GerenciadorES() {
 }
 
 void GerenciadorES::imprimeDistanciaPercorrida(std::string nomeArquivo )
-{   std::ifstream streamArquivo(nomeArquivo);
+{   
+    // Lendo arquivo de texto
+    std::ifstream streamArquivo(nomeArquivo);
     std::string entradaLida(
         (std::istreambuf_iterator<char>(streamArquivo)),
             std::istreambuf_iterator<char>());
-    std::cout << entradaLida;
+
+    // Divide números em vetores correspondentes
+    std::string s = entradaLida;
+    std::string delimitador = "\n";
+
+    size_t pos = 0;
+    std::vector<int> enderecosCilindrosListados;
+    std::string token;
+    int numeroToken;
+    while ((pos = s.find(delimitador)) != std::string::npos) {
+        token = s.substr(0, pos);
+        numeroToken = atoi(token.c_str());
+        enderecosCilindrosListados.push_back(numeroToken);
+        s.erase(0, pos + delimitador.length());
+    }
+    numeroToken = atoi(s.c_str());
+    enderecosCilindrosListados.push_back(numeroToken);
+    std::cout << enderecosCilindrosListados.size() << std::endl;
+
 }
