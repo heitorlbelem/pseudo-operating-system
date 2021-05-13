8e67e078f40538e8968e8e829f0193cb6df5bdd3
thiagolrpinho <thiagoluisrpinho@gmail.com>
2021-05-13 16:46:25 -0300

    :tada: add: GerenciadorES class header and implementation files
    Signed-off-by: thiagolrpinho <thiagoluisrpinho@gmail.com>

---
+++ include/gerenciador_es.hpp
@@ -0,0 +1,32 @@
+#ifndef gerenciador_es_hpp
+#define gerenciador_es_hpp
+/* 
+Neste módulo a equipe deve escrever um programa para simular o funcionamento dos principais algoritmos
+de escalonamento de disco estudados na disciplina. Os algoritmos de escalonamento de disco a serem
+implementados são os seguintes:
+• FCFS (First Come, First Serve);
+• SSF (ou SSTF – braço inicialmente para baixo);
+• SCAN.
+O programa deverá ler da entrada padrão um conjunto de número inteiros, no qual o primeiro número
+representa a quantidade de cilindros no disco, o segundo número representa o cilindro sobre o qual a cabeça
+de leitura do disco está inicialmente posicionada, e os demais representam uma sequência de requisições de
+acesso a serem atendidas, sempre um número por linha.
+O programa deverá imprimir na saída o número total de cilindros percorridos pela cabeça de leitura para
+atender todas as requisições solicitadas utilizando cada um dos algoritmos. 
+*/
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class GerenciadorES {
+    
+    private:
+    int tamanhoDisco;
+    public:
+    GerenciadorES(); // construtor
+    ~GerenciadorES(); // destrutor
+    void imprime_distancia_percorrida(std::string);
+};
+
+
+#endif
---
+++ src/gerenciador_es.cpp
@@ -0,0 +1,5 @@
+#include "gerenciador_es.hpp"
+
+void GerenciadorES::imprime_distancia_percorrida(std::string pai_careca ){
+    
+}
