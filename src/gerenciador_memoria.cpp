04375247cb8f4ffca7a2935e93e06cabfd4935ad
GabrielBessa <gabrielbessarofl@gmail.com>
2021-05-13 22:04:39 -0300

    Começando gerenciador de memoria

---
+++ src/gerenciador_memoria.cpp
@@ -0,0 +1,25 @@
+#include "gerenciador_memoria.hpp"
+
+std::ifstream t("file.txt");
+
+// construtor
+GerenciadorMemoria::GerenciadorMemoria() 
+{
+
+}
+// destrutor
+GerenciadorMemoria::~GerenciadorMemoria() 
+{
+
+}
+
+void GerenciadorMemoria::imprimeFaltaDePagina(std::vector<int> falta_de_pagina)
+{
+
+}
+
+int fifo(std::vector<int> sequencia_de_paginas)
+{
+    
+}
+
